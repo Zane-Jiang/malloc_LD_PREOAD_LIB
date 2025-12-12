5673ca10d9f84cbf8434699afea4bf764ebbfb8a
Zane-Jiang <2129056867@qq.com>
2025-12-12 12:59:07 +0800

    weight-interweave place v1

--- cxl_malloc_interweave/cxl_malloc.c
+++ cxl_malloc_interweave/cxl_malloc.c
@@ -55,36 +55,15 @@ static pthread_mutex_t seg_lock = PTHREAD_MUTEX_INITIALIZER;
 #define __unused __attribute__((unused))
 #define is_pow2(val) (((val) & ((val)-1)) == 0)
 
-/* global variables set by environment variables */
-static unsigned long maxnode;
-
-/* Interleave ratio configuration */
-static int g_local_ratio = 1;   /* Default: 1 page on local */
-static int g_remote_ratio = 1;  /* Default: 1 page on CXL */
-
-/* Arena indices for local and CXL nodes */
-static unsigned local_arena_index;
-static unsigned cxl_arena_index;
-static extent_hooks_t *local_hooks;
-static extent_hooks_t *cxl_hooks;
-static extent_hooks_t local_extent_hooks;
-static extent_hooks_t cxl_extent_hooks;
-static unsigned long local_nodemask;
-static unsigned long cxl_nodemask;
-
 
 /* Function declarations */
-void *interweave_malloc(size_t size, int local_ratio, int remote_ratio);
+void *interweave_malloc(size_t size);
 void interweave_free(void *ptr);
-void *interweave_calloc(size_t nmemb, size_t size, int local_ratio, int remote_ratio);
-void *interweave_realloc(void *ptr, size_t size, int local_ratio, int remote_ratio);
-void *interweave_aligned_alloc(size_t alignment, size_t size, int local_ratio, int remote_ratio);
-int interweave_posix_memalign(void **memptr, size_t alignment, size_t size, int local_ratio, int remote_ratio);
+void *interweave_calloc(size_t nmemb, size_t size);
+void *interweave_realloc(void *ptr, size_t size);
+void *interweave_aligned_alloc(size_t alignment, size_t size);
+int interweave_posix_memalign(void **memptr, size_t alignment, size_t sz);
 // size_t interweave_malloc_usable_size(void *ptr);
-
-void *local_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
-void *cxl_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
-int hmunmap(void *addr, size_t length);
 
 
 static void *(*libc_malloc)(size_t) = NULL;
@@ -98,53 +77,7 @@ static void *(*libc_memalign)(size_t, size_t) = NULL;
 static int (*libc_posix_memalign)(void **, size_t, size_t) = NULL;
 
 static size_t (*libc_malloc_usable_size)(void *) = NULL;
-
-
-static pthread_once_t jemalloc_once = PTHREAD_ONCE_INIT;
-static bool jemalloc_initialized = false;
-
-static void setup_jemalloc(void) {
-
-    size_t ulen = sizeof(unsigned);
-    int err __unused = mallctl("arenas.create", &local_arena_index, &ulen, NULL, 0);
-    assert(!err);
-    {
-        extent_hooks_t *hooks_ptr = &local_extent_hooks;
-        char key[64];
-        snprintf(key, sizeof(key), "arena.%u.extent_hooks", local_arena_index);
-        err = mallctl(key, NULL, NULL, &hooks_ptr, sizeof(hooks_ptr));
-        assert(!err);
-        local_hooks = &local_extent_hooks;
-    }
-
-    err = mallctl("arenas.create", &cxl_arena_index, &ulen, NULL, 0);
-    assert(!err);
-    {
-        extent_hooks_t *hooks_ptr = &cxl_extent_hooks;
-        char key[64];
-        snprintf(key, sizeof(key), "arena.%u.extent_hooks", cxl_arena_index);
-        err = mallctl(key, NULL, NULL, &hooks_ptr, sizeof(hooks_ptr));
-        assert(!err);
-        cxl_hooks = &cxl_extent_hooks;
-    }
-
-    jemalloc_initialized = true;
-    CXL_LOG("Initialized: local_arena=%u, cxl_arena=%u, ratio=%d:%d",
-            local_arena_index, cxl_arena_index, g_local_ratio, g_remote_ratio);
-}
-
-static inline bool ensure_jemalloc_ready(void) {
-    pthread_once(&jemalloc_once, setup_jemalloc);
-    return jemalloc_initialized;
-}
-
-
-/* Get interleave ratio from environment or use default */
-void get_interweave_ratio(int* local, int* remote)
-{
-    *local = g_local_ratio;
-    *remote = g_remote_ratio;
-}
+static void *(*libc_aligned_alloc)(size_t, size_t) = NULL;
 
 
 /* Record allocation segment */
@@ -206,23 +139,18 @@ size_t check_seg(unsigned long addr, int *is_interleaved)
     return size_to_free;
 }
 
-/* 覆盖 malloc_usable_size：若 ptr 属于我们的 interleaved 区段則返回记录的大小，
-   否则转发给 libc 的实现。 */
 size_t malloc_usable_size(void *ptr)
 {
-    /* 首先解析 libc 实现（按需） */
     if (unlikely(!libc_malloc_usable_size)) {
         libc_malloc_usable_size = (size_t (*)(void *))dlsym(RTLD_NEXT, "malloc_usable_size");
         if (!libc_malloc_usable_size) {
             libc_malloc_usable_size = (size_t (*)(void *))dlsym(RTLD_NEXT, "__malloc_usable_size");
         }
-        /* 如果都找不到，保守返回 0 */
         if (!libc_malloc_usable_size) {
             return 0;
         }
     }
 
-    /* 如果是我们记录的 segment，返回记录的长度 */
     pthread_mutex_lock(&seg_lock);
     struct addr_seg *seg = find_seg((unsigned long)ptr);
     size_t sz = seg ? seg_length(seg) : 0;
@@ -231,11 +159,10 @@ size_t malloc_usable_size(void *ptr)
     if (sz > 0)
         return sz;
 
-    /* 否则转发给 libc 实现 */
     return libc_malloc_usable_size(ptr);
 }
 
-/* 有些程序或 libc 内部可能直接调用 __malloc_usable_size，提供包装 */
+
 size_t __malloc_usable_size(void *ptr)
 {
     return malloc_usable_size(ptr);
@@ -248,11 +175,8 @@ void *malloc(size_t sz)
         CXL_LOG("malloc dlsym libc_malloc=%p", libc_malloc);
         if (!libc_malloc) return NULL;
     }
-    if (sz > INTERLEAVE_THRESHOLD && ensure_jemalloc_ready()) {
-        int local_ratio, remote_ratio;
-        get_interweave_ratio(&local_ratio, &remote_ratio);
-        
-        void *addr = interweave_malloc(sz, local_ratio, remote_ratio);
+    if (sz > INTERLEAVE_THRESHOLD) {
+        void *addr = interweave_malloc(sz);
         if (addr) {
             return addr;
         }
@@ -276,11 +200,8 @@ void *calloc(size_t nmemb, size_t size)
 
     size_t total = nmemb * size;
 
-    if (total > INTERLEAVE_THRESHOLD && ensure_jemalloc_ready()) {
-        int local_ratio, remote_ratio;
-        get_interweave_ratio(&local_ratio, &remote_ratio);
-        
-        void *addr = interweave_calloc(nmemb, size, local_ratio, remote_ratio);
+    if (total > INTERLEAVE_THRESHOLD) {
+        void *addr = interweave_calloc(nmemb, size);
         if (addr) {
             return addr;
         }
@@ -313,10 +234,8 @@ void *realloc(void *ptr, size_t size)
     }
     pthread_mutex_unlock(&seg_lock);
     
-    if (old_size > 0 && ensure_jemalloc_ready()) {
-        int local_ratio, remote_ratio;
-        get_interweave_ratio(&local_ratio, &remote_ratio);
-        return interweave_realloc(ptr, size, local_ratio, remote_ratio);
+    if (old_size > 0) {
+        return interweave_realloc(ptr, size);
     }
     
     return libc_realloc(ptr, size);
@@ -328,11 +247,8 @@ void *memalign(size_t align, size_t sz)
         libc_memalign = (void * (*)(size_t, size_t))dlsym(RTLD_NEXT, "memalign");
         if (!libc_memalign) return NULL;
     }
-    if (sz > INTERLEAVE_THRESHOLD && ensure_jemalloc_ready()) {
-        int local_ratio, remote_ratio;
-        get_interweave_ratio(&local_ratio, &remote_ratio);
-        
-        void *addr = interweave_aligned_alloc(align, sz, local_ratio, remote_ratio);
+    if (sz > INTERLEAVE_THRESHOLD) {
+        void *addr = interweave_aligned_alloc(align, sz);
         if (addr) {
             return addr;
         }
@@ -347,11 +263,8 @@ int posix_memalign(void **ptr, size_t align, size_t sz)
         libc_posix_memalign = (int (*)(void **, size_t, size_t))dlsym(RTLD_NEXT, "posix_memalign");
         if (!libc_posix_memalign) return ENOMEM;
     }
-    if (sz > INTERLEAVE_THRESHOLD && ensure_jemalloc_ready()) {
-        int local_ratio, remote_ratio;
-        get_interweave_ratio(&local_ratio, &remote_ratio);
-        
-        int ret = interweave_posix_memalign(ptr, align, sz, local_ratio, remote_ratio);
+    if (sz > INTERLEAVE_THRESHOLD) {
+        int ret = interweave_posix_memalign(ptr, align, sz);
         if (ret == 0) {
             return 0;
         }
@@ -404,43 +317,6 @@ int munmap(void *start, size_t length)
     return libc_munmap(start, length);
 }
 
-
-/* Extent hooks for local node */
-static void *local_extent_alloc(extent_hooks_t *extent_hooks __unused, void *new_addr, size_t size,
-                                size_t alignment __unused, bool *zero __unused, bool *commit __unused,
-                                unsigned arena_ind __unused) {
-    return local_mmap(new_addr, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-}
-
-static bool local_extent_dalloc(extent_hooks_t *extent_hooks __unused, void *addr, size_t size,
-                                bool committed __unused, unsigned arena_ind __unused) {
-    return hmunmap(addr, size) != 0;
-}
-
-static extent_hooks_t local_extent_hooks = {
-    .alloc = local_extent_alloc,
-    .dalloc = local_extent_dalloc,
-};
-
-/* Extent hooks for CXL node */
-static void *cxl_extent_alloc(extent_hooks_t *extent_hooks __unused, void *new_addr, size_t size,
-                              size_t alignment __unused, bool *zero __unused, bool *commit __unused,
-                              unsigned arena_ind __unused) {
-    return cxl_mmap(new_addr, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-}
-
-static bool cxl_extent_dalloc(extent_hooks_t *extent_hooks __unused, void *addr, size_t size,
-                              bool committed __unused, unsigned arena_ind __unused) {
-    return hmunmap(addr, size) != 0;
-}
-
-static extent_hooks_t cxl_extent_hooks = {
-    .alloc = cxl_extent_alloc,
-    .dalloc = cxl_extent_dalloc,
-};
-
-
-
 __attribute__((constructor)) void m_init(void) {
     libc_malloc = (void * (*)(size_t))dlsym(RTLD_NEXT, "malloc");
     libc_realloc = (void * (*)(void *, size_t))dlsym(RTLD_NEXT, "realloc");
@@ -451,67 +327,46 @@ __attribute__((constructor)) void m_init(void) {
     libc_mmap64 = (void * (*)(void *, size_t, int, int, int, off_t))dlsym(RTLD_NEXT, "mmap64");
     libc_memalign = (void * (*)(size_t, size_t))dlsym(RTLD_NEXT, "memalign");
     libc_posix_memalign = (int (*)(void **, size_t, size_t))dlsym(RTLD_NEXT, "posix_memalign");
-
-    setup_jemalloc();
-    maxnode = numa_max_node() + 1;
-    local_nodemask = 1UL << NUMA_LOCAL_NODE;
-    cxl_nodemask = 1UL << NUMA_CXL_NODE;
+    libc_aligned_alloc = (void * (*)(size_t, size_t))dlsym(RTLD_NEXT, "aligned_alloc");
  }
  
-/* mmap with binding to local node */
-void *local_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) {
-    void *new_addr = libc_mmap(addr, length, prot, flags, fd, offset);
-    if (unlikely(new_addr == MAP_FAILED))
-        return MAP_FAILED;
 
-    long ret = mbind(new_addr, length, MPOL_BIND, &local_nodemask, maxnode, 0);
-    if (unlikely(ret)) {
-        int mbind_errno = errno;
-        libc_munmap(new_addr, length);
-        errno = mbind_errno;
-        return MAP_FAILED;
-    }
-    return new_addr;
-}
-
-/* mmap with binding to CXL node */
-void *cxl_mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) {
-    void *new_addr = libc_mmap(addr, length, prot, flags, fd, offset);
-    if (unlikely(new_addr == MAP_FAILED))
-        return MAP_FAILED;
-
-    long ret = mbind(new_addr, length, MPOL_BIND, &cxl_nodemask, maxnode, 0);
-    if (unlikely(ret)) {
-        int mbind_errno = errno;
-        libc_munmap(new_addr, length);
-        errno = mbind_errno;
-        return MAP_FAILED;
-    }
-    return new_addr;
-}
-
-int hmunmap(void *addr, size_t length) {
-    return libc_munmap(addr, length);
-}
 
 
 /*
  * Interweave malloc: allocates memory and binds pages according to interleave ratio.
- * For example, with local_ratio=3, remote_ratio=2:
- *   Pages 0,1,2 -> local node
- *   Pages 3,4 -> CXL node
- *   Pages 5,6,7 -> local node
- *   Pages 8,9 -> CXL node
- *   ...
  */
-void *interweave_malloc(size_t size, int local_ratio, int remote_ratio)
+static void ensure_mapping_funcs(void)
 {
-    if (!ensure_jemalloc_ready() || remote_ratio == 0 || local_ratio == 0) {
-        CXL_LOG("jemalloc not ready, fallback to libc_malloc");
-        return libc_malloc(size);
+    if (unlikely(!libc_mmap)) {
+        libc_mmap = (void * (*)(void *, size_t, int, int, int, off_t))dlsym(RTLD_NEXT, "mmap");
     }
+    if (unlikely(!libc_munmap)) {
+        libc_munmap = (int (*)(void *, size_t))dlsym(RTLD_NEXT, "munmap");
+    }
+}
 
-    
+
+
+static int apply_interleave_policy(void *addr, size_t len)
+{
+    unsigned long nodemask = (1UL << NUMA_LOCAL_NODE) | (1UL << NUMA_CXL_NODE);
+    unsigned long maxnode = sizeof(nodemask) * 8;
+
+    if (mbind(addr, len, MPOL_WEIGHTED_INTERLEAVE, &nodemask, maxnode, MPOL_MF_MOVE) != 0) {
+        CXL_LOG("mbind interleave failed: %s", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+void *interweave_malloc(size_t size)
+{
+    ensure_mapping_funcs();
+    if (unlikely(!libc_mmap || !libc_munmap)) {
+        errno = ENOSYS;
+        return NULL;
+    }
     /* Allocate a contiguous virtual memory region */
     size_t aligned_size = (size + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
     void *base = libc_mmap(NULL, aligned_size, PROT_READ | PROT_WRITE, 
@@ -519,44 +374,21 @@ void *interweave_malloc(size_t size, int local_ratio, int remote_ratio)
     if (base == MAP_FAILED) {
         return NULL;
     }
-    
-    /* Calculate total pages and bind each page according to ratio */
-    size_t num_pages = aligned_size / PAGE_SIZE;
-    int cycle_length = local_ratio + remote_ratio;
-    
-    for (size_t i = 0; i < num_pages; i++) {
-        void *page_addr = (char *)base + (i * PAGE_SIZE);
-        int pos_in_cycle = i % cycle_length;
-        unsigned long *mask;
-        
-        if (pos_in_cycle < local_ratio) {
-            /* This page goes to local node */
-            mask = &local_nodemask;
-        } else {
-            /* This page goes to CXL node */
-            mask = &cxl_nodemask;
-        }
-        
-        long ret = mbind(page_addr, PAGE_SIZE, MPOL_BIND, mask, maxnode, MPOL_MF_MOVE);
-        if (ret != 0) {
-            CXL_LOG("mbind failed for page %zu: %s", i, strerror(errno));
-            /* Continue anyway, the page will use default policy */
-        }
+
+    if (apply_interleave_policy(base, aligned_size) != 0) {
+        libc_munmap(base, aligned_size);
+        return NULL;
     }
-    
+
     record_seg((unsigned long)base, aligned_size, 1);
     return base;
 }
 
 void interweave_free(void *ptr)
 {
-    if (unlikely(ptr == NULL))
+    ensure_mapping_funcs();
+    if (unlikely(!libc_munmap) || ptr == NULL)
         return;
-
-    if (!ensure_jemalloc_ready()) {
-        libc_free(ptr);
-        return;
-    }
 
     /* Find the segment to get the size */
     pthread_mutex_lock(&seg_lock);
@@ -572,7 +404,7 @@ void interweave_free(void *ptr)
     return ;
 }
 
-void *interweave_calloc(size_t nmemb, size_t size, int local_ratio, int remote_ratio)
+void *interweave_calloc(size_t nmemb, size_t size)
 {
     if (size != 0 && nmemb > SIZE_MAX / size) {
         errno = ENOMEM;
@@ -580,7 +412,7 @@ void *interweave_calloc(size_t nmemb, size_t size, int local_ratio, int remote_r
     }
     
     size_t total = nmemb * size;
-    void *ptr = interweave_malloc(total, local_ratio, remote_ratio);
+    void *ptr = interweave_malloc(total);
     
     if (likely(ptr)) {
         memset(ptr, 0, total);
@@ -588,13 +420,14 @@ void *interweave_calloc(size_t nmemb, size_t size, int local_ratio, int remote_r
     return ptr;
 }
 
-void *interweave_realloc(void *ptr, size_t size, int local_ratio, int remote_ratio)
+void *interweave_realloc(void *ptr, size_t size)
 {
-    if (!ensure_jemalloc_ready())
-        return libc_realloc(ptr, size);
-    
+    ensure_mapping_funcs();
+    if (unlikely(!libc_munmap))
+        return NULL;
+
     if (ptr == NULL)
-        return interweave_malloc(size, local_ratio, remote_ratio);
+        return interweave_malloc(size);
     
     if (size == 0) {
         interweave_free(ptr);
@@ -613,7 +446,7 @@ void *interweave_realloc(void *ptr, size_t size, int local_ratio, int remote_rat
     }
     
     /* Allocate new memory */
-    void *new_ptr = interweave_malloc(size, local_ratio, remote_ratio);
+    void *new_ptr = interweave_malloc(size);
     if (!new_ptr)
         return NULL;
     
@@ -628,11 +461,13 @@ void *interweave_realloc(void *ptr, size_t size, int local_ratio, int remote_rat
     return new_ptr;
 }
 
-void *interweave_aligned_alloc(size_t alignment, size_t size, int local_ratio, int remote_ratio)
+void *interweave_aligned_alloc(size_t alignment, size_t size)
 {
-    if (!ensure_jemalloc_ready())
-        return libc_memalign(alignment, size);
-    
+    ensure_mapping_funcs();
+    if (unlikely(!libc_mmap || !libc_munmap)) {
+        errno = ENOSYS;
+        return NULL;
+    }
     if (unlikely(alignment == 0 || !is_pow2(alignment))) {
         errno = EINVAL;
         return NULL;
@@ -666,53 +501,47 @@ void *interweave_aligned_alloc(size_t alignment, size_t size, int local_ratio, i
         libc_munmap((void *)(aligned_addr + aligned_size), end_excess);
     }
     
-    /* Apply interleave policy */
-    size_t num_pages = aligned_size / PAGE_SIZE;
-    int cycle_length = local_ratio + remote_ratio;
-    
-    if (cycle_length > 0) {
-        for (size_t i = 0; i < num_pages; i++) {
-            void *page_addr = (char *)aligned_ptr + (i * PAGE_SIZE);
-            int pos_in_cycle = i % cycle_length;
-            unsigned long *mask = (pos_in_cycle < local_ratio) ? &local_nodemask : &cxl_nodemask;
-            mbind(page_addr, PAGE_SIZE, MPOL_BIND, mask, maxnode, MPOL_MF_MOVE);
-        }
+    if (apply_interleave_policy(aligned_ptr, aligned_size) != 0) {
+        libc_munmap(aligned_ptr, aligned_size);
+        return NULL;
     }
-    
+
     record_seg((unsigned long)aligned_ptr, aligned_size, 1);
     return aligned_ptr;
 }
 
-int interweave_posix_memalign(void **memptr, size_t alignment, size_t sz, int local_ratio, int remote_ratio)
+void *aligned_alloc(size_t alignment, size_t size)
 {
-    if (unlikely(alignment == 0 || !is_pow2(alignment) || alignment % sizeof(void *) != 0)) {
-        *memptr = NULL;
+    if (unlikely(!libc_aligned_alloc)) {
+        libc_aligned_alloc = (void * (*)(size_t, size_t))dlsym(RTLD_NEXT, "aligned_alloc");
+        if (!libc_aligned_alloc) {
+            errno = ENOSYS;
+            return NULL;
+        }
+    }
+    if (size > INTERLEAVE_THRESHOLD) {
+        void *addr = interweave_aligned_alloc(alignment, size);
+        if (addr) {
+            return addr;
+        }
+        CXL_LOG("interweave_aligned_alloc failed, falling back to libc aligned_alloc");
+    }
+    return libc_aligned_alloc(alignment, size);
+}
+
+int interweave_posix_memalign(void **memptr, size_t alignment, size_t sz)
+{
+    if (!memptr || alignment < sizeof(void *) || alignment % sizeof(void *) || !is_pow2(alignment)) {
         return EINVAL;
     }
-    
-    *memptr = interweave_aligned_alloc(alignment, sz, local_ratio, remote_ratio);
-    
-    if (*memptr == NULL) {
+
+    void *ptr = interweave_aligned_alloc(alignment, sz);
+    if (!ptr) {
         return ENOMEM;
     }
+
+    *memptr = ptr;
     return 0;
 }
 
-// size_t interweave_malloc_usable_size(void *ptr)
-// {
-//     if (unlikely(ptr == NULL))
-//         return 0;
-    
-//     pthread_mutex_lock(&seg_lock);
-//     struct addr_seg *seg = find_seg((unsigned long)ptr);
-//     size_t size = seg ? seg_length(seg) : 0;
-//     pthread_mutex_unlock(&seg_lock);
-    
-//     if (size > 0)
-//         return size;
-
-//     if (ensure_jemalloc_ready())
-//         return je_sallocx(ptr, 0);
-//     return 0;
-// }
 
